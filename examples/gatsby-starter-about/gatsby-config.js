accd7b0c70033f0ba0b9affca656279a8e2a210d
joellesenne <jfkles@me.com>
2021-07-02 19:54:18 +0200

    :pencil2: fix typo && refactor project

--- examples/gatsby-starter-about/gatsby-config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  plugins: [`@joellesenne/gatsby-theme-style`],
-}
--- examples/gatsby-starter-projects/src/pages/index.js
+++ examples/gatsby-starter-projects/src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
 
-export default function App(props) {
+export default function App() {
   return <h1>Projects</h1>
 }
